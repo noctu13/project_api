630c0098878ed3423378a8cd1733d04ba1b1a5f5
noctu13 <13bit57@gmail.com>
2023-12-01 18:54:16 +0300

    correct query, no day%-age check!

--- api/views.py
+++ api/views.py
@@ -39,20 +39,13 @@ class EventsView(generics.ListAPIView):
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         day = self.kwargs.get('day')
-        event_dict = {}
-        for key in short_type_dict.keys():
-             event_dict[key] = short_type == short_type_dict[key]
         if year and month and day:
             query_day = datetime(year, month, day, 0, 0, 0, 
                 tzinfo=timezone.utc)
-            query1 = Q(start_time__lt=query_day + timedelta(days=1))
+            query1 = Q(start_time__lte=query_day + timedelta(days=1))
             query2 = Q(end_time__gt=query_day)
             into_query_day = Polyline.objects.filter(
                 query1 & query2).order_by('start_time')
-            if event_dict['PML']:
-                noon = query_day + timedelta(hours=12)
-                query1 = Q(start_time__lte=noon)
-                query2 = Q(end_time__gt=noon)
             queryset = events_query.filter(query1 & query2).order_by(
                 'start_time').prefetch_related(
                 Prefetch(
@@ -61,7 +54,7 @@ class EventsView(generics.ListAPIView):
                     to_attr='into_query_day'
                 )
             )
-            if not queryset and event_dict['PML']:
+            if not queryset and short_type == short_type_dict['PML']:
                 queryset = events_query.latest('start_time')
                 queryset.into_query_day = queryset.polyline
                 queryset = [queryset]
